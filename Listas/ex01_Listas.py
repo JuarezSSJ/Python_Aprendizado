b1f3ad66f7df44926f7b20e6cf847c8fdbac49dd
Juarez Santana <100575590+JuarezSSJ@users.noreply.github.com>
2022-12-22 18:31:41 -0300

    Trabalhando com Listas e aplicando para casos micros

--- Listas/ex01_Listas.py
+++ Listas/ex01_Listas.py
@@ -4,8 +4,33 @@ emails = ["juarez@gmail.com",
           "bola@hotmail.com",
           "fogo@furia.com",
           "gol@hotmail.com"]
-lis = []
-for x in emails:
-    i = emails.index(x)
-    lis.append(x)
-print(lis)
+
+#lista.append = funciona para cadastrar item na lista
+#lista.index(item) = pesquisa na lista e informa qual o indice daquele item
+
+print(emails)
+
+#remomendo um item da lista
+emails.remove("juarezssj@gmail.com")
+print(emails)
+
+#tamanho = len(lista) = informa o tamanho da lista
+
+#lista1.extend(lista2) = modifica a lista1 colocando os item da lista2
+#ou lista_nova = lista1 + lista2
+
+#ordenarlista - lista.sort()
+emails.sort()
+print(emails)
+
+#Print das listas
+#método join = lista.join(separador)
+
+print("\n".join(emails))
+
+#separando um texto e transformando em lista
+
+texto = "mesa, cadeira, fogão, bola, computador, loja, tela, tv"
+lista_tratada = texto.split(", ")
+
+print("\n",lista_tratada)
---
+++ Listas/ex02_Listas.py
@@ -0,0 +1,25 @@
+"""
+Faturamento do Melhor e do Pior Mês do Ano
+Qual foi o valor de vendas do melhor mês do Ano? E valor do pior mês do ano?
+"""
+meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
+vendas_1sem = [25000, 29000, 22200, 17750, 15870, 19900]
+vendas_2sem = [19850, 20120, 17540, 15555, 49051, 9650]
+lista_porcent_ano = []
+
+vendas_ano = vendas_1sem + vendas_2sem #unindo as 2 listas
+
+melhor_mes = vendas_ano.index(max(vendas_ano)) #max = retorna a posição com o maior valor
+
+pior_mes = vendas_ano.index(min(vendas_ano)) #min = retorna a posição com o menor valor
+
+soma_ano = sum(vendas_ano) #sum = soma todos os valores da lista
+
+#obj.: percorre a lista vendas_ano e encontrar quanto esse valor representou no fat do ano
+
+for valor in vendas_ano:
+    lista_porcent_ano.append(valor/soma_ano*100)
+
+print("\nO mês de {} foi o de melhor faturamento R${:.2f} do ano e representou {:.2f}% do fat.".format(meses[melhor_mes],vendas_ano[melhor_mes],lista_porcent_ano[melhor_mes]))
+
+print("\nO mês de {} foi o pior faturamento R${:.2f} do ano e representou {:.2f}% do fat.".format(meses[pior_mes],vendas_ano[pior_mes], lista_porcent_ano[pior_mes]))
