cc23f0f47f90cb666cd6c79a20090488d92e32a6
Juarez Santana <100575590+JuarezSSJ@users.noreply.github.com>
2022-12-26 13:14:17 -0300

    Option Button

---
+++ TKinter/tela_optionbutton.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+janela = tk.Tk()
+
+janela.title("Criando Option Button")
+
+dicionario_cotacoes = {
+    "Dólar": 5.50,
+    "Euro": 6.70,
+    "Bitcoin": 20000,
+}
+
+mensagem = tk.Label(text="Escolha a moeda: ")
+mensagem.grid(row=0, column=0, columnspan=3)
+
+# por definição o Radiobutton quando criado já vem marcado
+# para resolver isso temos que passar um valor para a variavel (value="Nada"), resolve isso
+var_moeda = tk.StringVar(value="Nada")
+
+
+def enviar():
+    # print(var_moeda.get())
+    moeda_preenchida = var_moeda.get()
+    cotacao_moeda = dicionario_cotacoes.get(moeda_preenchida)
+    # apos o click será criado um label para apresentar a mensagem para o usuario
+    mensagem_cotacao = tk.Label(text="")
+    mensagem_cotacao.grid(row=3, column=0, columnspan=3)
+    if cotacao_moeda:
+        mensagem_cotacao["text"] = f"Cotação do {moeda_preenchida} é de {cotacao_moeda} reais"
+
+
+botao_euro = tk.Radiobutton(
+    text="Euro", variable=var_moeda, value="Euro", command=enviar)
+botao_dolar = tk.Radiobutton(
+    text="Dólar", variable=var_moeda, value="Dólar", command=enviar)
+botao_bitcoin = tk.Radiobutton(
+    text="Bitcoin", variable=var_moeda, value="Bitcoin", command=enviar)
+botao_euro.grid(row=1, column=0)
+botao_dolar.grid(row=1, column=1)
+botao_bitcoin.grid(row=1, column=2)
+
+
+# botao_enviar = tk.Button(text="Enviar", command=enviar)
+# botao_enviar.grid(row=1, column=0)
+
+janela.mainloop()
